6b2cdbba8efbf18578c661730e43ca7b7dcae07e
TheLBall <outofmario1000@gmail.com>
2017-09-17 19:50:25 -0400

    Update format.sh

--- format.sh
+++ format.sh
@@ -1,3 +1,14 @@
 dialog --yesno "Do you want to format hard drive?" 10 20
-dialog --msgbox "Formating..." 10 20
+clear
+echo "Formatting the hard drive whether or not you wanted to."
+sleep 5
 dialog --msgbox "Formatted hard drive." 10 20
+clear
+echo Reinstalling the OS...
+sleep 5
+dialog --msgbox "Reinstalled the OS." 10 20
+clear
+echo Wait a sec...
+sleep 5
+dialog --msgbox "It was just a prank! Stop taking things so seriously!" 10 50
+clear
